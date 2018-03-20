c2f7d8b1589fa7eb8a72852da90ba14256295b89
Joao Paulo Pereira Zanetti <jppzanetti@gmail.com>
2018-03-20 09:40:50 -0300

    Getting input from command line in count_intermediates.py.

--- count/count_intermediates.py
+++ count/count_intermediates.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from math import factorial, ceil, floor
+import sys
+import argparse
 
 def cycle_intermediates(k):
     """Number of intermediates for a k-cycle."""
@@ -24,5 +26,38 @@ def count_intermediates(cycles = [], paths = []):
     return total
 
 if __name__ == '__main__':
-    print(count_intermediates(paths = [6]))
-    print(count_intermediates(cycles = [6]))
+    parser = argparse.ArgumentParser(description='Counts number of intermediates.')
+
+    parser.add_argument('-c', nargs='*', type=int, default=[],
+                        help='list of lengths of cycles')
+    parser.add_argument('-p', nargs='*', type=int, default=[],
+                        help='list of lengths of paths')
+
+    if len(sys.argv) == 1:
+        args = parser.parse_args('-h'.split())
+    else:
+        args = parser.parse_args()
+
+        k_cycles = []
+    for k in args.c:
+        if k < 0:
+            print("ERROR: Negative cycle size.")
+            exit(-1)
+        if k == 0:
+            print("ERROR: Null cycle size.")
+            exit(-1)
+        if k % 2 == 1:
+            print("ERROR: Odd cycle size.")
+            exit(-1)
+        if k > 2:
+            k_cycles.append(k)
+
+    k_paths = []
+    for k in args.p:
+        if k < 0:
+            print("ERROR: Negative path length.")
+            exit(-1)
+        if k > 0:
+            k_paths.append(k)
+
+    print(count_intermediates(cycles = k_cycles, paths = k_paths))
